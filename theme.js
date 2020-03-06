f40c6863e1cfd3e3539c55a10aca6fdf9dda95c6
Jack Pritchard <jack@noface.co.uk>
2020-03-06 17:13:27 +0000

    Write deck MVP

--- theme.js
+++ theme.js
@@ -1,7 +1,46 @@
-import theme from "mdx-deck/themes";
-import Provider from "./Provider";
+import { themes } from "mdx-deck";
 
 export default {
-	...theme,
-	Provider
+	...themes.dark,
+	fonts: {
+		body: "Roboto, sans-serif",
+		monospace: '"Roboto Mono", monospace'
+	},
+	colors: {
+		background: "#011627",
+		code: "#7fdbca",
+		link: "#CCC",
+		primary: "#7fdbca",
+		text: "white"
+	},
+	css: {
+		textAlign: "left",
+		fontSize: "16px",
+		"@media screen and (min-width:64em)": {
+			fontSize: "32px"
+		},
+		"& h3": {
+			maxWidth: "60ch"
+		},
+		"& p": {
+			maxWidth: "35ch"
+		},
+		"& blockquote": {
+			fontSize: "16px"
+		},
+		"& blockquote p": {
+			maxWidth: "100%"
+		},
+		"& .prism-code": {
+			fontSize: "40px",
+			padding: "62.5px 0px !important"
+		},
+		"& h1 + div": {
+			flexGrow: "0 !important"
+		},
+		"& h1 + div ~ p": {
+			maxWidth: "55ch",
+			width: "55ch"
+		}
+	}
 };
